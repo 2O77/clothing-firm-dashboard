f998a25158e86d93b3c033c38392940a5e4aae70
Ertugrul Akdag <ertugrulakdag@gmail.com>
2024-10-06 21:19:23 +0300

    Türkiye İl Sınırlarını GeoJSON ile Leaflet Haritasına Eklemek ve popup yapmak

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
     <ul class="tabs">
       <li :class="{ active: activeTab === 1 }" @click="activeTab = 1">Basit Harita</li>
       <li :class="{ active: activeTab === 2 }" @click="activeTab = 2">Özelleştirilmiş Pin</li>
-      <li :class="{ active: activeTab === 3 }" @click="activeTab = 3">Dimaik oluşan harita</li>
-      <li :class="{ active: activeTab === 4 }" @click="activeTab = 4">Haritada gruplanan pinler </li>
-      <li :class="{ active: activeTab === 5 }" @click="activeTab = 5">Türkiye İl Sınırları </li>
+      <li :class="{ active: activeTab === 3 }" @click="activeTab = 3">Dimaik  harita</li>
+      <li :class="{ active: activeTab === 4 }" @click="activeTab = 4">Gruplanan pinler </li>
+      <li :class="{ active: activeTab === 5 }" @click="activeTab = 5">İl Sınırları </li>
+      <li :class="{ active: activeTab === 6 }" @click="activeTab = 6">İl Sınırları V2</li>
     </ul>
 
     <!-- Tab içeriği -->
@@ -16,6 +17,7 @@
       <DynamicMapComponent v-if="activeTab === 3" />
       <InteractiveMapComponent v-if="activeTab === 4" />
       <TurkiyeCitiesMapComponent v-if="activeTab === 5" />
+      <TurkiyeCitiesV2MapComponent v-if="activeTab === 6" />
     </div>
   </div>
 </template>
@@ -27,6 +29,7 @@ import CustomIconComponent from './components/CustomIconComponent.vue'
 import DynamicMapComponent from './components/DynamicMapComponent.vue'
 import InteractiveMapComponent from './components/InteractiveMapComponent.vue'
 import TurkiyeCitiesMapComponent from './components/TurkiyeCitiesMapComponent.vue'
+import TurkiyeCitiesV2MapComponent from './components/TurkiyeCitiesV2MapComponent.vue'
 
 const activeTab = ref(1)
 </script>
---
+++ src/components/TurkiyeCitiesV2MapComponent.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <h2>Harita lokasyon verisi milliemlak.gov.tr üzerinden alınmıştır.</h2>
+    <p>Türkiye'de şu an Satışa sunulan taşınmaz ilanladır.</p>
+    <div class="map" id="map"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const map = ref(null)
+const apiUrl = 'https://mebis-cbs-s-p.csb.gov.tr/Proxy/index?url=http%3A%2F%2Fmebis-geo-01.csb.gov.tr%2Fgeoserver%2FMEOP%2Fwfs%3Frequest%3DGetFeature%26version%3D2.0.0%26srsName%3DEPSG%3A3857%26outputFormat%3Dapplication%252Fjson%26typeNames%3DMEOP%3AMILEWEB_SATIS%26CQL_FILTER%3D1%3D1%20and%20ihale_tarihi%20%3E%3D%202024-10-06T00%3A00%3A00.000Z'
+const geoJsonUrl = '/tr-cities-utf8.json'  // Public klasöründen GeoJSON dosyasına erişim Kaynak:https://github.com/cihadturhan/tr-geojson
+
+const loadMap = () => {
+  map.value = L.map('map').setView([39.9334, 32.8597], 6)
+
+  // OpenStreetMap katmanını ekle
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© <a href="https://github.com/ertugrulakdag/vue3-map-leafletjs" target="_blank">github.com/ertugrulakdag/vue3-map-leafletjs</a>'
+  }).addTo(map.value)
+
+  // İl sınırları GeoJSON dosyasını yükleyip haritaya ekleyin
+  loadGeoJsonLayer()
+}
+
+const loadData = async () => {
+  try {
+    const response = await fetch(apiUrl)
+    if (!response.ok) {
+      throw new Error("Veri alınırken hata oluştu.")
+    }
+    
+    const geoJsonData = await response.json()
+
+    // Her feature için bir marker oluştur
+    geoJsonData.features.forEach(feature => {
+      const coordinates = feature.geometry.coordinates
+      const yuzolcumu = feature.properties.yuzolcumu
+      const tasinmaz_no = feature.properties.tasinmaz_no
+      const ihale_tarihi = formatDate(feature.properties.ihale_tarihi)
+
+      // Leaflet'te uzunluk ve enlem ters olduğu için koordinatları ters çeviriyoruz
+      const latLng = L.Projection.SphericalMercator.unproject(L.point(coordinates))
+
+      // Marker ekle
+      L.marker([latLng.lat, latLng.lng]).addTo(map.value)
+        .bindPopup(`Yüzölçümü: ${yuzolcumu} m² <br>İhale Tarihi: ${ihale_tarihi}<br>Taşınmaz No: ${tasinmaz_no} `)
+    })
+  } catch (error) {
+    console.error("Veri yüklenirken hata oluştu:", error)
+  }
+}
+
+// Türkiye İl sınırları GeoJSON katmanını haritaya ekleyen fonksiyon
+const loadGeoJsonLayer = async () => {
+  try {
+    const response = await fetch(geoJsonUrl)
+    if (!response.ok) {
+      throw new Error("İl sınırları GeoJSON verisi yüklenirken hata oluştu.")
+    }
+
+    const geoJsonData = await response.json()
+
+    // GeoJSON verisini haritaya ekleyin, hover özellikleri ekleyin
+    L.geoJSON(geoJsonData, {
+      style: {
+        color: '#ff7800',  // Sınır rengi (açık turuncu)
+        weight: 3.1,       // Çizgi kalınlığı
+        opacity: 0.8,      // Çizgi şeffaflığı
+        fillColor: '#ffd39b', // Dolgu rengi (saydam turuncu)
+        fillOpacity: 0.2   // Dolgu şeffaflığı
+      },
+      onEachFeature: (feature, layer) => {
+        layer.on({
+          mouseover: (e) => showPopup(e, feature),
+          mouseout: hidePopup
+        })
+      }
+    }).addTo(map.value)
+
+  } catch (error) {
+    console.error("İl sınırları GeoJSON verisi yüklenirken hata oluştu:", error)
+  }
+}
+
+// İl üzerine gelindiğinde popup'ı gösterme
+const showPopup = (e, feature) => {
+  const layer = e.target
+
+  // Popup oluştur ve sağ üstteki x işaretini kaldırarak göster
+  layer.bindPopup(feature.properties.name, { closeButton: false }).openPopup()
+
+  // Katmanı belirginleştir
+  layer.setStyle({
+    weight: 3,
+    color: '#666',
+    fillOpacity: 0.7
+  })
+}
+
+// İl üzerinden çıkıldığında popup'ı kapatma ve stili sıfırlama
+const hidePopup = (e) => {
+  const layer = e.target
+
+  // Popup'ı kapat
+  layer.closePopup()
+
+  // Stil ayarlarını geri yükle
+  layer.setStyle({
+    weight: 1.5,
+    color: '#ff7800',
+    fillOpacity: 0.3
+  })
+}
+
+// ISO tarih formatını dd.MM.yyyy formatına dönüştüren fonksiyon
+const formatDate = (isoDateString) => {
+  const date = new Date(isoDateString)
+  
+  if (isNaN(date.getTime())) {
+    return 'Hatalı Tarih'
+  }
+
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+
+  return `${day}.${month}.${year}`
+}
+
+onMounted(() => {
+  loadMap()
+  loadData()
+})
+</script>
+
+<style scoped>
+ 
+</style>
