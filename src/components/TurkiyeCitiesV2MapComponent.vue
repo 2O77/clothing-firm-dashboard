47feaabb46b7efcef2dfa83be435cd9b462bd24b
Ertugrul Akdag <ertugrulakdag@gmail.com>
2024-10-06 21:38:24 +0300

    fix

--- src/components/TurkiyeCitiesV2MapComponent.vue
+++ src/components/TurkiyeCitiesV2MapComponent.vue
@@ -16,7 +16,14 @@ const apiUrl = 'https://mebis-cbs-s-p.csb.gov.tr/Proxy/index?url=http%3A%2F%2Fme
 const geoJsonUrl = '/tr-cities-utf8.json'  // Public klasöründen GeoJSON dosyasına erişim Kaynak:https://github.com/cihadturhan/tr-geojson
 
 const loadMap = () => {
-  map.value = L.map('map').setView([39.9334, 32.8597], 6)
+  map.value = L.map('map', {
+    center: [39.9334, 32.8597],  // Harita merkez koordinatları
+    zoom: 6,                     // Varsayılan zoom seviyesi
+    minZoom: 5,                  // En düşük zoom seviyesi
+    maxZoom: 10,                 // En yüksek zoom seviyesi
+    zoomDelta: 0.5,              // Zoom artırma adımı
+    zoomSnap: 0.5                // Zoom snap adımı
+  })
 
   // OpenStreetMap katmanını ekle
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
