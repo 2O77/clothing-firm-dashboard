21491c1fcc520a9de5705e10695d295551e74a83
2O77 <recepahmetkara@gmail.com>
2024-11-07 22:33:16 +0300

    map initialized

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,13 @@
 <template>
-  <div id="app">
-    <!-- Tab başlıkları -->
-    <ul class="tabs">
-      <li :class="{ active: activeTab === 1 }" @click="activeTab = 1">Basit Harita</li>
-      <li :class="{ active: activeTab === 2 }" @click="activeTab = 2">Özelleştirilmiş Pin</li>
-      <li :class="{ active: activeTab === 3 }" @click="activeTab = 3">Dimaik  harita</li>
-      <li :class="{ active: activeTab === 4 }" @click="activeTab = 4">Gruplanan pinler </li>
-      <li :class="{ active: activeTab === 5 }" @click="activeTab = 5">İl Sınırları </li>
-      <li :class="{ active: activeTab === 6 }" @click="activeTab = 6">İl Sınırları V2</li>
-    </ul>
-
-    <!-- Tab içeriği -->
-    <div class="tab-content">
-      <SimpleMapComponent v-if="activeTab === 1" />
-      <CustomIconComponent v-if="activeTab === 2" />
-      <DynamicMapComponent v-if="activeTab === 3" />
-      <InteractiveMapComponent v-if="activeTab === 4" />
-      <TurkiyeCitiesMapComponent v-if="activeTab === 5" />
-      <TurkiyeCitiesV2MapComponent v-if="activeTab === 6" />
-    </div>
+  <div class="dashboard-page">
+    <TurkiyeCitiesMapComponent />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import SimpleMapComponent from './components/SimpleMapComponent.vue'
-import CustomIconComponent from './components/CustomIconComponent.vue'
-import DynamicMapComponent from './components/DynamicMapComponent.vue'
-import InteractiveMapComponent from './components/InteractiveMapComponent.vue'
-import TurkiyeCitiesMapComponent from './components/TurkiyeCitiesMapComponent.vue'
-import TurkiyeCitiesV2MapComponent from './components/TurkiyeCitiesV2MapComponent.vue'
 
-const activeTab = ref(6)
+import TurkiyeCitiesMapComponent from './components/TurkiyeCitiesMapComponent.vue'
+
 </script>
 
-<style scoped>
-/* Stil güncellemeleri */
-.tabs {
-  display: flex;
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-  border-bottom: 2px solid #1cbd12;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;  
-  background-color: #fff;
-  z-index: 1000;  
-}
-
-.tabs li {
-  padding: 15px 20px;
-  cursor: pointer;
-  color: #666;
-  border-bottom: 3px solid transparent;  
-  font-weight: bold;
-  margin-right: 10px;
-}
-
-.tabs li.active {
-  border-bottom: 3px solid #5c967c;  
-  color: #42b983;
-}
-
-.tab-content {
-  margin-top: 40px;  
-  padding: 1px;
-  width: 100%;
-  height: calc(100vh - 60px); 
-}
-
- 
-</style>
+<style scoped></style>
--- src/components/TurkiyeCitiesMapComponent.vue
+++ src/components/TurkiyeCitiesMapComponent.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <h2>Harita lokasyon verisi milliemlak.gov.tr üzerinden alınmıştır.</h2>
-    <p>Türkiye'de şu an Satışa sunulan taşınmaz ilanladır.</p>
     <div class="map" id="map"></div>
   </div>
 </template>
@@ -10,19 +8,14 @@
 import { ref, onMounted } from 'vue'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-const customIcon = L.icon({
-  iconUrl: '/icon-32.png', // İkonun URL'si
-  iconAnchor: [19, 38], // İkonun haritada sabitleneceği nokta
-  popupAnchor: [-8, -35] // Popup konumu
-});
+
 const map = ref(null)
-const apiUrl = 'https://mebis-cbs-s-p.csb.gov.tr/Proxy/index?url=http%3A%2F%2Fmebis-geo-01.csb.gov.tr%2Fgeoserver%2FMEOP%2Fwfs%3Frequest%3DGetFeature%26version%3D2.0.0%26srsName%3DEPSG%3A3857%26outputFormat%3Dapplication%252Fjson%26typeNames%3DMEOP%3AMILEWEB_SATIS%26CQL_FILTER%3D1%3D1%20and%20ihale_tarihi%20%3E%3D%202024-10-06T00%3A00%3A00.000Z'
-const geoJsonUrl = '/tr-cities-utf8.json'  // Public klasöründen GeoJSON dosyasına erişim Kaynak:https://github.com/cihadturhan/tr-geojson
+const geoJsonUrl = '/tr-cities-utf8.json'  // Türkiye il sınırlarını içeren GeoJSON dosyasının yolu
 
 const loadMap = () => {
   map.value = L.map('map', {
-    center: [39.9334, 32.8597],  // Harita merkez koordinatları
-    zoom: 6,                     // Varsayılan zoom seviyesi
+    center: [38.9637, 35.2433],  // Türkiye'nin ortalama koordinatları
+    zoom: 6.8,                     // Varsayılan zoom seviyesi
     minZoom: 5,                  // En düşük zoom seviyesi
     maxZoom: 10,                 // En yüksek zoom seviyesi
     zoomDelta: 0.5,              // Zoom artırma adımı
@@ -38,34 +31,6 @@ const loadMap = () => {
   loadGeoJsonLayer()
 }
 
-const loadData = async () => {
-  try {
-    const response = await fetch(apiUrl)
-    if (!response.ok) {
-      throw new Error("Veri alınırken hata oluştu.")
-    }
-
-    const geoJsonData = await response.json()
-
-    // Her feature için bir marker oluştur
-    geoJsonData.features.forEach(feature => {
-      const coordinates = feature.geometry.coordinates
-      const yuzolcumu = feature.properties.yuzolcumu
-      const tasinmaz_no = feature.properties.tasinmaz_no
-      const ihale_tarihi = formatDate(feature.properties.ihale_tarihi)
-
-      // Leaflet'te uzunluk ve enlem ters olduğu için koordinatları ters çeviriyoruz
-      const latLng = L.Projection.SphericalMercator.unproject(L.point(coordinates))
-
-      // Marker ekle
-      L.marker([latLng.lat, latLng.lng], { icon: customIcon }).addTo(map.value)
-        .bindPopup(`Yüzölçümü: ${yuzolcumu} m² <br>İhale Tarihi: ${ihale_tarihi}<br>Taşınmaz No: ${tasinmaz_no} `)
-    })
-  } catch (error) {
-    console.error("Veri yüklenirken hata oluştu:", error)
-  }
-}
-
 // Türkiye İl sınırları GeoJSON katmanını haritaya ekleyen fonksiyon
 const loadGeoJsonLayer = async () => {
   try {
@@ -75,40 +40,34 @@ const loadGeoJsonLayer = async () => {
     }
 
     const geoJsonData = await response.json()
+    console.log('GeoJSON Verisi:', geoJsonData)  // Veriyi kontrol etmek için konsola yazdırıyoruz
 
+    // GeoJSON verisini haritaya ekleyin, tıklama özelliklerini ekleyin
     L.geoJSON(geoJsonData, {
       style: {
-        color: '#ff0000',  // Sınır rengi (Kırmızı)
-        weight: 3,       // Çizgi kalınlığı
+        color: '#ff7800',  // Sınır rengi (açık turuncu)
+        weight: 3.1,       // Çizgi kalınlığı
         opacity: 0.8,      // Çizgi şeffaflığı
         fillColor: '#ffd39b', // Dolgu rengi (saydam turuncu)
-        fillOpacity: 0.1   // Dolgu şeffaflığı
+        fillOpacity: 0.2   // Dolgu şeffaflığı
+      },
+      onEachFeature: (feature, layer) => {
+        // Tıklama olayını tanımlıyoruz
+        layer.on('click', (e) => {
+          // Tıklanan ilin adını popup'ta göster
+          layer.bindPopup(`İl: ${feature.properties.name} <br> Diğer Veri: ${feature.properties.someOtherData}`)
+            .openPopup()
+        })
       }
     }).addTo(map.value)
 
   } catch (error) {
-    console.error("İl sınırları GeoJSON verisi yüklenirken hata oluştu:", error)
+    console.error("GeoJSON verisi yüklenirken hata oluştu:", error)
   }
-}
-
-// ISO tarih formatını dd.MM.yyyy formatına dönüştüren fonksiyon
-const formatDate = (isoDateString) => {
-  const date = new Date(isoDateString)
-
-  if (isNaN(date.getTime())) {
-    return 'Hatalı Tarih'
-  }
-
-  const day = String(date.getDate()).padStart(2, '0')
-  const month = String(date.getMonth() + 1).padStart(2, '0')
-  const year = date.getFullYear()
-
-  return `${day}.${month}.${year}`
 }
 
 onMounted(() => {
   loadMap()
-  loadData()
 })
 </script>
 
